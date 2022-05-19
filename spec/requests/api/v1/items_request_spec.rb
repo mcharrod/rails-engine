e380614785407e1e00a029f3469e2b70c7894e01
Katy Harrod <m.catherine.harrod@gmail.com>
2022-05-19 13:49:25 -0600

    Add items controller

---
+++ spec/requests/api/v1/items_request_spec.rb
@@ -0,0 +1,91 @@
+require 'rails_helper'
+
+
+describe "Item API" do
+
+  # let blocks for actions to do when their name is called
+  # let! will do the specified action immediately
+
+  let(:parsed) { JSON.parse(response.body, symbolize_names: true)}
+  let(:data) { parsed[:data] }
+  let!(:merchants) { create_list(:merchant, 3) }
+
+
+# shared examples is tests that can be re-used between different contexts
+  shared_examples 'Adheres to JSON API spec' do
+    it 'it adheres to JSON API spec' do
+      expect(parsed).to have_key(:data)
+      expect(data).to be_a(klass)
+    end
+
+    it "returns status 200" do
+      expect(response).to be_successful
+    end
+  end
+
+# contexts are describes basically
+  context 'GET #index' do
+
+    # creating the variable for use in the shared example
+    let(:klass) {Array}
+    before do
+      get api_v1_items_path
+    end
+
+     # calling teh shared behavior
+    it_behaves_like 'Adheres to JSON API spec'
+    it 'returns proper data' do
+      (0..2).all? do |num|
+        # every single resource returned matches the created object
+        data[num][:id] == item[num].id &&
+          data[num][:type] == "item" &&
+            data[num][:attributes][:unit_price] == items[num].unit_price
+              data[num][:attributes][:name] == items[num].description
+                data[num][:attributes][:merchant_id] == items[num].merchant_id
+      end
+    end
+  end
+
+  context 'GET #show' do
+    let(:klass) {Hash}
+    let(:merchant) {merchants.first}
+    before do
+      get api_v1_merchant_path(merchant.id)
+    end
+    it_behaves_like 'Adheres to JSON API spec'
+
+    it 'has a merchant name' do
+      expect(data[:attributes][:name]).to eq(merchant.name)
+    end
+  end
+
+  context 'GET items #index' do
+    it_behaves_like 'Adheres to JSON API spec'
+    before do
+      create(:merchant)
+      create_list(:item, 3, merchant: merchant)
+      get "/api/v1/merchants/#{merchant.id}/items"
+    end
+    let(:merchant) {merchants.first}
+    let(:klass) {Array}
+    it 'returns list of items' do
+      expect(data.first[:type]).to eq("merchant_item")
+    end
+
+
+    # it 'returns proper data' do
+    #   (0..2).all? do |num|
+    #     # every single resource returned matches the created object
+    #     data[num][:id] == merchants[num].id &&
+    #       data[num][:type] == "merchant" &&
+    #         data[num][:attributes][:name] == merchants[num].name
+    #   end
+    # end
+
+
+
+    # make sure each thing is type: item
+
+    #
+  end
+end
